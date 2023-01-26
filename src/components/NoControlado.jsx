1bb24ae767db6fcc1d48ab3dbd8c11b873d69930
Franco Rodrigo Zabala Nogales <fzabala@khronoscloud.com>
2023-01-26 18:35:49 -0500

    estructura form y on submit

--- src/components/NoControlado.jsx
+++ src/components/NoControlado.jsx
@@ -1,4 +1,30 @@
-const NoControlado=()=>{
-  return'no controlado'
-}
-export default NoControlado
+const NoControlado = () => {
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    console.log("de");
+  };
+  return (
+    <form onSubmit={handlerSubmit}>
+      <input
+        type="text"
+        placeholder="Ingrese todo"
+        className="form-control mb-2"
+        name="title"
+      />
+      <textarea
+        className="form-control mb-2"
+        type="text"
+        placeholder="Ingrese descripcion"
+        name="descripcion"
+      />
+      <select className="form-select" name="state" defaultValue="completado">
+        <option value="pendiente">Pendiente</option>
+        <option value="completado">Completado</option>
+      </select>
+      <button className="btn btn-primary" type="submit">
+        Agregar
+      </button>
+    </form>
+  );
+};
+export default NoControlado;
